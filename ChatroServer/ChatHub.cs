99de6a00f3c81317163c68b563734ba186f4ea51
MBalous <m.balouse@gmail.com>
2017-01-12 21:58:58 +0100

    Initial commit

---
+++ ChatroServer/ChatHub.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace ChatroServer
+{
+    public class ChatHub : Hub
+    {
+        public override Task OnConnected()
+        {
+            Debug.WriteLine($"Client connected - connection id: {this.Context.ConnectionId}", "info");
+            return base.OnConnected();
+        }
+
+        public override Task OnDisconnected(bool stopCalled)
+        {
+            Debug.WriteLine($"Client disconnected - connection id: {this.Context.ConnectionId}", "info");
+            return base.OnDisconnected(stopCalled);
+        }
+
+        public void NewBroadcast(string message)
+        {
+            this.Clients.All.sendBroadcast(message);
+        }
+
+        public void NewMessage(string message, string recipient)
+        {
+
+        }
+
+        public void JoinRoom(string roomName)
+        {
+
+        }
+    }
+}
