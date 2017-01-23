1fdb6d821e918de0abb5a399db1222856d531ec0
MBalous <m.balouse@gmail.com>
2017-01-24 00:17:28 +0100

    User entity edited
    Aded ToString() override.

--- ChatroServer/src/Entity/User.cs
+++ ChatroServer/src/Entity/User.cs
@@ -1,11 +1,35 @@
-﻿using ServiceStack.DataAnnotations;
+﻿using System;
+using ServiceStack.DataAnnotations;
 
-namespace ChatroServer
+namespace ChatroServer.Entity
 {
     public class User
     {
         [PrimaryKey]
         public string Username { get; set; }
+
         public string Password { get; set; }
+
+        public User(string username, string password)
+        {
+            if (username != null)
+            {
+                this.Username = username;
+            }
+            else
+            {
+                throw new ArgumentNullException(nameof(username));
+            }
+            this.Password = password;
+        }
+        
+        /// <summary>
+        /// Get username.
+        /// </summary>
+        /// <returns>Returns username.</returns>
+        public override string ToString()
+        {
+            return this.Username;
+        }
     }
 }
