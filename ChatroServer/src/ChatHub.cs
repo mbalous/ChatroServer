24c99336e9f212539a6380e77025c9ce8e56141b
MBalous <m.balouse@gmail.com>
2017-01-24 00:19:25 +0100

    Historical message downloading

--- ChatroServer/src/ChatHub.cs
+++ ChatroServer/src/ChatHub.cs
@@ -3,16 +3,14 @@ using System.Collections.Generic;
 using System.Data;
 using System.Diagnostics;
 using System.Linq;
-using System.Reflection;
 using System.Threading.Tasks;
+using ChatroServer.Entity;
 using Microsoft.AspNet.SignalR;
-using ServiceStack;
-using ServiceStack.Text;
 using ServiceStack.OrmLite;
 
 namespace ChatroServer
 {
-    public class ChatHub : Hub
+    public sealed class ChatHub : Hub
     {
         public static OrmLiteConnectionFactory ConnectionFactory { get; }
         private static readonly Dictionary<string, User> ConnectionIdsUsers = new Dictionary<string, User>();
@@ -31,6 +29,7 @@ namespace ChatroServer
             }
         }
 
+        /// <inheritdoc />
         public override Task OnConnected()
         {
             Debug.WriteLine($"Client connected - connection id: {this.Context.ConnectionId}", "info");
@@ -38,6 +37,7 @@ namespace ChatroServer
             return base.OnConnected();
         }
 
+        /// <inheritdoc />
         public override Task OnDisconnected(bool stopCalled)
         {
             Debug.WriteLine($"Client disconnected - connection id: {this.Context.ConnectionId}", "info");
@@ -45,35 +45,67 @@ namespace ChatroServer
             return base.OnDisconnected(stopCalled);
         }
 
-        
-        public void SendBroadcast(string message)
+
+        public void SendBroadcast(string content)
         {
             User username = ConnectionIdsUsers[this.Context.ConnectionId];
             if (username == null)
             {
                 throw new HubException("Username not set, can't send anything.");
             }
-            this.Clients.All.NewBroadcast(message, username);
+            Message message = new Message(DateTime.Now, username, content);
+            using (IDbConnection db = ConnectionFactory.Open())
+            {
+                db.Save(message);
+            }
+            this.Clients.All.NewBroadcast(content, username);
         }
 
         public void SendMessage(string message, string recipient)
         {
+            if (!IsLogged())
+            {
+                // TODO
+                return;
+            }
+
             User sender = ConnectionIdsUsers[this.Context.ConnectionId];
-            if (sender == null)
+            User userRecipient;
+            using (IDbConnection db = ConnectionFactory.Open())
             {
-                throw new HubException("Username not set, can't send anything.");
+                userRecipient = db.Select<User>().FirstOrDefault(user => user.Username == recipient);
             }
-            // find recipient
-            string recipientId = ConnectionIdsUsers.FirstOrDefault(pair => pair.Value.Username == recipient).Key;
-            if (recipientId == null)
+            if (userRecipient == null)
             {
-                // user not found
-                throw new HubException($"User {recipient} not found...");
+                // TODO: Figure out how to notify the client about this...
+                throw new Exception($"User {recipient} not found in database...");
             }
-            // user found sending message
-            this.Clients.Client(ConnectionIdsUsers.First(pair => pair.Value.Username == recipient).Key);
+
+            Message msg = new Message(DateTime.Now, sender, recipient);
+
+            // Is the user online?
+            string connectionId;
+            bool isConnected = TryGetUserConnectionIdFromUsername(recipient, out connectionId);
+            if (isConnected)
+            {
+                this.Clients.Client(connectionId).NewMessage(message, sender.Username);
+            }
         }
-        
+
+        private bool TryGetUserConnectionIdFromUsername(string username, out string connectionId)
+        {
+            string result = ConnectionIdsUsers.FirstOrDefault(pair => pair.Value.Username == username).Key;
+            if (result != null)
+            {
+                connectionId = result;
+                return true;
+            }
+            else
+            {
+                connectionId = null;
+                return false;
+            }
+        }
 
         public LoginResult Login(string username, string password)
         {
@@ -88,22 +120,40 @@ namespace ChatroServer
                 {
                     return LoginResult.Failure;
                 }
-                else
+                User userDb = result.First();
+                if (userDb.Password == password)
                 {
-                    User userDb = result.First();
-                    return userDb.Password == password ? LoginResult.Success : LoginResult.Failure;
+                    ConnectionIdsUsers[this.Context.ConnectionId] = userDb;
+                    return LoginResult.Success;
                 }
+                return LoginResult.Failure;
             }
         }
-    }
 
-    public enum LoginResult
-    {
-        Success,
-        Failure
+        public List<Message> DownloadMessages(DateTime from)
+        {
+            if (!IsLogged())
+            {
+                return null;
+            }
+
+            using (IDbConnection db = ConnectionFactory.Open())
+            {
+                List<Message> messages = db.Select<Message>().Where(message => message.TimeStamp > @from).ToList();
+                return messages;
+            }
+        }
+
         private bool IsLogged()
         {
             return ConnectionIdsUsers.Keys.Any(s => s == this.Context.ConnectionId);
+        }
+
+
+        public enum LoginResult
+        {
+            Success,
+            Failure
         }
     }
 }
